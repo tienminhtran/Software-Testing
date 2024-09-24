16ec508307141b0cad7b5071cfeba2b8cb92fd03
H51M14\Student <Student@gmail.com>
2024-09-24 16:49:45 +0700

    first commit

---
+++ UnitTestProject1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("590ccdaf-8e4e-4bf8-87b4-c771dfc44ecc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTestProject1/UnitTest3.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool expectedResutl = true;
+            bool actualResutl = o.primeCheck(37);
+            Assert.AreEqual(expectedResutl, actualResutl);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool expectedResutl = false;
+            bool actualResutl = o.primeCheck(25);
+            Assert.AreEqual(expectedResutl, actualResutl);
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool expectedResutl = false;
+            bool actualResutl = o.primeCheck(1);
+            Assert.AreEqual(expectedResutl, actualResutl);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool expectedResutl = true;
+            bool actualResutl = o.primeCheck(2);
+            Assert.AreEqual(expectedResutl, actualResutl);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool expectedResutl = false;
+            bool actualResutl = o.primeCheck(0);
+            Assert.AreEqual(expectedResutl, actualResutl);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+            bool expectedResutl = false;
+            bool actualResutl = o.primeCheck(1000);
+            Assert.AreEqual(expectedResutl, actualResutl);
+        }
+    }
+}
